66ee5f10106a1ab7d9f95ef1b7a937ba60bb666b
DamianGapinski <dgapinski2@gmail.com>
2026-09-11 10:00:39 +0200

    Games i Contact

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ContactPage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>test</h1>
+    </div>
+  );
+}
